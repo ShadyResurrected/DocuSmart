9772c1c508bdc2832ff54c071f0dd7de3d0932b3
ShadyResurrected <cchandra554@gmail.com>
2023-11-07 19:28:08 +0530

    authorization using kinde
    Signed-off-by: ShadyResurrected <cchandra554@gmail.com>

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/server"
+
+
+const Page = async () => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
+
+    return (
+        <div>{user.email}</div>
+    )
+}
+
+export default Page
