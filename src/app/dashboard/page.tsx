835ad8d063fdbd5c4afa419cce874096befc1d01
ShadyResurrected <cchandra554@gmail.com>
2023-11-07 23:09:21 +0530

    setting up trpc,prisma db on neon, and flow for new and exsiting users
    Signed-off-by: ShadyResurrected <cchandra554@gmail.com>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,25 @@
+import DashBoard from "@/components/DashBoard"
+import { db } from "@/db"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/server"
+import { redirect } from "next/navigation"
 
 
 const Page = async () => {
     const { getUser } = getKindeServerSession()
     const user = await getUser()
 
+    if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+
+    const dbUser = db.user.findFirst({
+        where: {
+            id: user.id
+        }
+    })
+
+    if (!dbUser) redirect('/auth-callback?origin=dashboard')
+
     return (
-        <div>{user.email}</div>
+        <DashBoard />
     )
 }
 
