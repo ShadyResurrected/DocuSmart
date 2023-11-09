6c2be10b41234ea4d176fb33bf23a5013342c1f4
ShadyResurrected <cchandra554@gmail.com>
2023-11-09 20:21:05 +0530

    completed loading states of pdf
    Signed-off-by: ShadyResurrected <cchandra554@gmail.com>

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -3,7 +3,9 @@
 import Messages from "./Messages"
 import ChatInput from "./ChatInput"
 import { trpc } from "@/app/_trpc/client"
-import { Loader2 } from "lucide-react"
+import { ChevronLeft, Loader2, XCircle } from "lucide-react"
+import Link from "next/link"
+import { buttonVariants } from "../ui/button"
 
 interface ChatWrapperProps {
     fileId: string
@@ -32,6 +34,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
                     </p>
                 </div>
             </div>
+            <ChatInput isDisabled />
         </div>
     )
 
@@ -46,6 +49,26 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
                     </p>
                 </div>
             </div>
+            <ChatInput isDisabled />
+        </div>
+    )
+
+    if (data?.status === "FAILED") return (
+        <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+            <div className="flex-1 flex justify-center items-center flex-col mb-28">
+                <div className="flex flex-col items-center gap-2">
+                    <XCircle className="h-8 w-8 text-red-500" />
+                    <h3 className="font-semibold text-xl">Too many pages in PDF</h3>
+                    <p className="text-zinc-500 text-sm">
+                        Your <span className="font-medium">Free</span> plan supports up to 5 pages per PDF.
+                    </p>
+                    <Link href='/dashboard' className={buttonVariants({
+                        variant: 'secondary',
+                        className: "mt-4"
+                    })}><ChevronLeft className="h-3 w-3 mr-1.5" />Back</Link>
+                </div>
+            </div>
+            <ChatInput isDisabled />
         </div>
     )
 
@@ -54,8 +77,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
             <div className="flex-1 justify-between flex flex-col mb-28">
                 <Messages />
             </div>
-
-            <ChatInput isDisabled />
+            <ChatInput />
         </div>
     )
 }
